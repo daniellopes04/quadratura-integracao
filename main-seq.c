61931898a5d7fc09e16e671e6fdcd454cbc4b8a2
Daniel Lopes <daniel.lopes04@gmail.com>
2019-10-28 15:30:11 -0300

    # tempos de execucao de cada funcao

--- main-seq.c
+++ main-seq.c
@@ -20,35 +20,31 @@ double func6(double x){ return cos(exp(-x)) * x;}
 double func7(double x){ return cos(exp(-x)) * ((0.005 * pow(x, 3)) + 1); }
 
 //funcao recursiva para aproximacao do valor da integral
+
 double retangulosRecursiva(double (*func)( double), double limiteA, double limiteB, double erro, double integral, double funcEmA, double funcEmB, double funcEmMedio) {
 
-  double valorMedio;                              //variavel para guardar o ponto médio no intervalo [A, B]
-  double dist;                                    //variavel para guardar a distancia de A para B
-  double valorMedioEsquerda, valorMedioDireita;   //variaveis para guardarem os pontos médios no intervalo [A, C] e [C, B]
-  double funcaoEmEsquerda, funcaoEmDireita;       //variáveis para guardarem os valores da função avaliadas no ponto médio entre [A, C] e [C, B]
-  double integralEsquerda, integralDireita;       //variáveis para calcular a integral no lado esquerdo e direito do intervalo [A, C] e [C, B] pelo método dos retangulos
-  double novaIntegral;                            //variável que guarda a nova aproximação da integral da parte dividida;
+  double valorMedio;                                    //variavel para guardar o ponto médio no intervalo [A, B]
+  double dist;                                          //variavel para guardar a distancia de A para B
+  double valorMedioEsquerda, valorMedioDireita;         //variaveis para guardarem os pontos médios no intervalo [A, C] e [C, B]
+  double funcaoEmEsquerda, funcaoEmDireita;             //variáveis para guardarem os valores da função avaliadas no ponto médio entre [A, C] e [C, B]
+  double integralEsquerda, integralDireita;             //variáveis para calcular a integral no lado esquerdo e direito do intervalo [A, C] e [C, B] pelo método dos retangulos
+  double novaIntegral;                                  //variável que guarda a nova aproximação da integral da parte dividida;
 
-  valorMedio = (limiteA + limiteB) / 2;           //calcula o ponto médio entre A e B
-  dist = limiteB - limiteA;                       //calcula a distancia entre A e B
+  valorMedio = (limiteA + limiteB) / 2;                 //calcula o ponto médio entre A e B
+  dist = limiteB - limiteA;                             //calcula a distancia entre A e B
 
-  valorMedioEsquerda = (limiteA + valorMedio) / 2;    //calcula o ponto médio entre A e C
-  valorMedioDireita = (valorMedio + limiteB) / 2;     //calcula o ponto médio entre C e B
+  valorMedioEsquerda = (limiteA + valorMedio) / 2;      //calcula o ponto médio entre A e C
+  valorMedioDireita = (valorMedio + limiteB) / 2;       //calcula o ponto médio entre C e B
 
-  funcaoEmEsquerda = func(valorMedioEsquerda);        //calcula a função avaliada no ponto medio entre A e C
-  funcaoEmDireita = func(valorMedioDireita);          //calcula a função avaliada no ponto medio entre C e B
+  funcaoEmEsquerda = func(valorMedioEsquerda);          //calcula a função avaliada no ponto medio entre A e C
+  funcaoEmDireita = func(valorMedioDireita);            //calcula a função avaliada no ponto medio entre C e B
 
-  integralEsquerda = ( dist / 2 ) * funcaoEmEsquerda; //calcula a integral da parte da esquerda (de A até C)
-  integralDireita = ( dist / 2 ) * funcaoEmDireita;   //calcula a integral da parte da direita (de C até B)
+  integralEsquerda = ( dist / 2 ) * funcaoEmEsquerda;   //calcula a integral da parte da esquerda (de A até C)
+  integralDireita = ( dist / 2 ) * funcaoEmDireita;     //calcula a integral da parte da direita (de C até B)
   
   novaIntegral = integralEsquerda + integralDireita;                                                                       
   
-  if (fabs(novaIntegral - integral) <= 15 * erro) { //Calculo do método da quadratura adaptiva para saber se o novo valor da integral satisfaz o 
-                                                               //erro determinado no inicio do problema, caso satisfaça, esse valor é usado para servir como
-                                                               //aproximação do valor da integral da função, caso contratário, é chamado mais duas instancias
-                                                               //da função recursiva, onde irão separar o lado esquerdo e o lado direito e fazer o mesmo processo,
-                                                               //dividindo em esses lados em 2 novos lados até que o erro minimo seja satisfeito, fazendo assim uma
-                                                               //série de recursões e divindo cada vez mais a integral em pequenos retangulos nas partes mais cŕticias
+  if (fabs(novaIntegral - integral) <= 15 * erro) { 
     return novaIntegral + (novaIntegral - integral) / 15;
   }                                              
   
@@ -68,11 +64,11 @@ double retangulosInicial(double (*func) (double), double limiteA, double limiteB
   valorMedio = (limiteA + limiteB)/2;     //ponto médio entre A e B
   dist = limiteB - limiteA;               //distancia entre A e B                                    
   
-  funcEmA = func(limiteA);          //função no ponto A
-  funcEmB = func(limiteB);          //função no ponto B
-  funcEmMedio = func(valorMedio);   //função no ponto médio
+  funcEmA = func(limiteA);                //função no ponto A
+  funcEmB = func(limiteB);                //função no ponto B
+  funcEmMedio = func(valorMedio);         //função no ponto médio
 
-  integral = dist * funcEmMedio;    //utilizando a regra dos retangulos, calcula-se uma aproximação para a integral                                                            
+  integral = dist * funcEmMedio;          //utilizando a regra dos retangulos, calcula-se uma aproximação para a integral                                                            
   
   return retangulosRecursiva(func, limiteA, limiteB, erro, integral, funcEmA, funcEmB, funcEmMedio); 
 
@@ -94,26 +90,99 @@ int main(int argc, char *argv[]) {
   b = atoi(argv[2]);
   maxError = atof(argv[3]);
 
+  //executa todas as funcoes calculando o tempo de execucao de cada uma
+  //-----------------------------------------------------------------------------------
+  GET_TIME(inicio);
+
+  printf("Resultado para a função 1 = %f\n", retangulosInicial(func1, a, b, maxError));
+
+  GET_TIME(fim);
+
+  //calcula e exibe o tempo gasto com a multiplicacao
+  delta = fim - inicio;
+  printf("Tempo de calculo da integral: %.8lf\n\n", delta);
+  //-----------------------------------------------------------------------------------
+  
+
+  //-----------------------------------------------------------------------------------
+  GET_TIME(inicio);
+
+  if (a >= -1 && b <= 1) printf("Resultado para a função 2 = %f\n", retangulosInicial(func2, a, b, maxError));
+
+  GET_TIME(fim);
+
+  //calcula e exibe o tempo gasto com a multiplicacao
+  delta = fim - inicio;
+  printf("Tempo de calculo da integral: %.8lf\n\n", delta);
+  //-----------------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------------------
+  GET_TIME(inicio);
+
+  printf("Resultado para a função 3 = %f\n", retangulosInicial(func3, a, b, maxError));
+
+  GET_TIME(fim);
+
+  //calcula e exibe o tempo gasto com a multiplicacao
+  delta = fim - inicio;
+  printf("Tempo de calculo da integral: %.8lf\n\n", delta);
+  //-----------------------------------------------------------------------------------
+
+
   //-----------------------------------------------------------------------------------
 
   GET_TIME(inicio);
 
-  printf("Resultado para a função 1 = %f\n", retangulosInicial(func1, a, b, maxError));
-  if (a >= -1 && b <= 1) printf("Resultado para a função 2 = %f\n", retangulosInicial(func2, a, b, maxError));
-  printf("Resultado para a função 3 = %f\n", retangulosInicial(func3, a, b, maxError));
   printf("Resultado para a função 4 = %f\n", retangulosInicial(func4, a, b, maxError));
+
+  GET_TIME(fim);
+
+  //calcula e exibe o tempo gasto com a multiplicacao
+  delta = fim - inicio;
+  printf("Tempo de calculo da integral: %.8lf\n\n", delta);
+  //-----------------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------------------
+
+  GET_TIME(inicio);
+
   printf("Resultado para a função 5 = %f\n", retangulosInicial(func5, a, b, maxError));
+
+  GET_TIME(fim);
+
+  //calcula e exibe o tempo gasto com a multiplicacao
+  delta = fim - inicio;
+  printf("Tempo de calculo da integral: %.8lf\n\n", delta);
+  //-----------------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------------------
+
+  GET_TIME(inicio);
+
   printf("Resultado para a função 6 = %f\n", retangulosInicial(func6, a, b, maxError));
+
+  GET_TIME(fim);
+
+  //calcula e exibe o tempo gasto com a multiplicacao
+  delta = fim - inicio;
+  printf("Tempo de calculo da integral: %.8lf\n\n", delta);
+  //-----------------------------------------------------------------------------------
+
+
+  //-----------------------------------------------------------------------------------
+  GET_TIME(inicio);
+
   printf("Resultado para a função 7 = %f\n", retangulosInicial(func7, a, b, maxError));
 
   GET_TIME(fim);
 
-  //-----------------------------------------------------------------------------------
-
   //calcula e exibe o tempo gasto com a multiplicacao
   delta = fim - inicio;
-  
   printf("Tempo de calculo da integral: %.8lf\n", delta);
+  //-----------------------------------------------------------------------------------
 
   return 0;
 }
