b3d54938cac6c875002beeff01c11350adcc4d0b
Daniel Lopes <daniel.lopes04@gmail.com>
2019-11-18 20:14:42 -0200

    adicionando segundo trabalho

---
+++ leitores-escritores/main-conc.c
@@ -0,0 +1,17 @@
+/*
+ * Disciplina: Computacao Concorrente
+ * Prof.: Silvana Rossetto
+ * Trabalho 2
+ * Codigo: Problema de leitores e escritores sem inanicao
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+//funcao principal
+int main(int argc, char *argv[]) {  
+
+  return 0;
+}
